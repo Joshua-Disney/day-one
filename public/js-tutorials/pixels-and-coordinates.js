f49780d3e38ae65551369c278d48f331adccfc2e
Joshua-Disney <joshua.a.disney@gmail.com>
2021-11-28 16:51:41 -0700

    Pixel explanations

---
+++ public/js-tutorials/pixels-and-coordinates.js
@@ -0,0 +1,6 @@
+// In CodeGuppy the canvas is 800 pixels wide and 600 pixels high
+// This means we have 800 x 600 = 480,000 total pixels on the canvas.
+// We refer to these numbers as the resolution of the canvas.
+
+// CodeGuppy considers the first pixel as being the one in the upper-left corner. This pixel has coordinates (0, 0).
+// CodeGuppy considers the first pixel as being the one in the upper-left corner. This pixel has coordinates (0, 0).
