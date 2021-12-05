1c2f83dc1b4777b2c1faa59a7a21b9f77caeedde
Joshua-Disney <joshua.a.disney@gmail.com>
2021-12-05 01:44:01 -0700

    Build first interactive game in Phiser(Red Light/Green Light)

--- src/codecademy-tutorials/codecademy-day-one.js
+++ src/codecademy-tutorials/codecademy-day-one.js
@@ -179,3 +179,52 @@ const config = {
 }
 
 const game = new Phaser.Game(config)
+
+
+// INTERACTIVE GAMEPLAY
+const gameState = {
+    onColor: 0xaaffaa,
+    offColor: 0xffaaaa,
+  }
+  
+  function create() {
+    gameState.rect1 = this.add.rectangle(200, 100, 100, 100, gameState.onColor);
+    gameState.rect2 = this.add.rectangle(200, 300, 100, 100, gameState.offColor);
+    
+    // add the switchedOn state here
+    gameState.switchedOn = true
+    // // set gameState.rect2 as interactive here
+    gameState.rect1.setInteractive()
+    gameState.rect2.setInteractive()
+    // create the pointerup listener for rect2 here
+    gameState.rect1.on("pointerup", function() {
+      gameState.switchedOn = true
+    })
+    gameState.rect2.on("pointerup", function() {
+      gameState.switchedOn = false
+    })
+   
+  }
+  
+  function update() {
+    if (gameState.switchedOn === true) {
+      gameState.rect1.fillColor = gameState.onColor,
+      gameState.rect2.fillColor = gameState.offColor
+    } else {
+      gameState.rect1.fillColor = gameState.offColor,
+      gameState.rect2.fillColor = gameState.onColor
+    }
+  }
+  
+  const config = {
+    type: Phaser.AUTO,
+    width: 400,
+    height: 450,
+    backgroundColor: 0x333333,
+    scene: {
+      create,
+      update
+    }
+  }
+  
+  const game = new Phaser.Game(config)
