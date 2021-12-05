530fd582aa38865c84281c5e0fcc587ad3c35cab
Joshua-Disney <joshua.a.disney@gmail.com>
2021-12-04 22:56:46 -0700

    Bring over starting code from codecademy

--- src/codecademy-tutorials/codecademy-day-one.js
+++ src/codecademy-tutorials/codecademy-day-one.js
@@ -1,1 +1,20 @@
-// This is where my code will go
+function create() {
+    // Change "Codey's Adventures\n  in Code World" to the name of your game
+    this.add.text(50, 100, "Codey's Adventures\n  in Code World", { font: "40px Times New Roman", fill: "#ffa0d0"});
+  
+    // Change "by Codecademy" to your name!
+    this.add.text(130, 300, "by Codecademy", { font: "20px Times New Roman", fill: "#ffa0d0"});
+  }
+  
+  const config = {
+      type: Phaser.AUTO,
+      width: 450,
+      height: 600,
+      backgroundColor: "#5f2a55",
+      scene: {
+      create
+      }
+  };
+  
+  const game = new Phaser.Game(config);
+  
