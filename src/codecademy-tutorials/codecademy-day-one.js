f84d7e54c1f9ea57016c3753ea19b8658d158ba1
Joshua-Disney <joshua.a.disney@gmail.com>
2021-12-09 00:04:07 -0700

    Put it all together from scratch, end of lesson

--- src/codecademy-tutorials/codecademy-day-one.js
+++ src/codecademy-tutorials/codecademy-day-one.js
@@ -318,3 +318,40 @@ const config = {
 }
 
 const game = new Phaser.Game(config)
+
+// PUT IT ALL TOGETHER FROM SCRATCH
+const gameState = {}
+
+function preload() {
+  this.load.image('codey', 'https://content.codecademy.com/courses/learn-phaser/codey.png')
+}
+
+function create() {
+  gameState.codey = this.add.sprite(300, 300, 'codey')
+  gameState.cursors = this.input.keyboard.createCursorKeys()
+}
+
+function update() {
+  if (gameState.cursors.down.isDown) {
+    gameState.codey.y += 1
+  } else if (gameState.cursors.up.isDown) {
+    gameState.codey.y -= 1
+  } else if (gameState.cursors.right.isDown) {
+    gameState.codey.x += 1
+  } else if (gameState.cursors.left.isDown) {
+    gameState.codey.x -= 1
+  }
+}
+
+const config = {
+  width: 600,
+  height: 600,
+  backgroundColor: '#7ef9ff',
+  scene: {
+    preload,
+    create,
+    update
+  }
+}
+
+const game = new Phaser.Game(config)
