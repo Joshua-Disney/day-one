65bfabfb4d834baf6272abee16bf994ae9f83684
Joshua-Disney <joshua.a.disney@gmail.com>
2022-01-13 01:28:12 -0700

    Add StartsScene

--- src/codecademy-tutorials/phaser-physics/codeys-counting-conundrum/StartScene.js
+++ src/codecademy-tutorials/phaser-physics/codeys-counting-conundrum/StartScene.js
@@ -5,7 +5,7 @@ class StartScene extends Phaser.Scene {
   
     preload() {
       this.load.spritesheet('searching', 'https://content.codecademy.com/courses/learn-phaser/Counting/codey_searching.png', { frameWidth: 300, frameHeight: 300 });
-    }
+    } /* End of preload() */
   
     create() {
       // Creates the text on the start screen:
@@ -13,16 +13,23 @@ class StartScene extends Phaser.Scene {
       this.add.text(130, 520, ' Ready Count?\nClick to start!', { fill: '#4D39E0', fontSize: '20px' });
   
       // Create the sprite object, animation, and play the animation: 
+          gameState.startSprite = this.add.sprite(220, 320, 'searching')
               
-              
-              
+          this.anims.create({
+        key: 'codeySearch',
+        frames: this.anims.generateFrameNames('searching',
+        { start: 0, end: 5}),
+        delay: 0,
+        frameRate: 2,
+        repeat: -1
+      })
+      gameState.startSprite.anims.play('codeySearch')
+  
       this.input.on('pointerup', () => {
         // Add logic to transition from StartScene to GameScene:
               this.scene.stop('StartScene')
         this.scene.start('GameScene')
       });
-  
-  
-    }
+    } /* End of create() */
   }
   
